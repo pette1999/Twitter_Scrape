01dc58bffe23b996606c16b26fd65880b9664f67
pette1999 <haichen@chapman.edu>
2022-03-19 04:19:02 -0700

    now can send direct message and pull my own following list:

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Twitter_Scrape
+
+people.csv - People we found, waiting to follow
+following.csv - Current following
+blacklist.csv - people who got removed from our following list
--- scrape.py
+++ scrape.py
@@ -3,6 +3,7 @@ from decouple import config
 import json
 import csv
 import helper
+import time
 
 bearer_token = config('bearerToken',default='')
 consumer_key = config('consumer_key',default='')
@@ -21,7 +22,8 @@ def getClientV2():
                         consumer_key=consumer_key, 
                         consumer_secret=consumer_secret, 
                         access_token=access_token, 
-                        access_token_secret=access_token_secret)
+                        access_token_secret=access_token_secret,
+                        wait_on_rate_limit=True)
   
   return client
 
@@ -37,16 +39,27 @@ def getTweets(tag):
   tweets = [tweet._json for tweet in tweet_cursor]
   return tweets
 
-def getFollowers():
-  client = getClientV2()
-  userID = getUserInfo('chen_haifan')
-  followers = client.get_users_followers(id=userID)
-  return followers.data
-
-def getMe():
+def getFollowers_by_username(username):
   api = getAPIV1()
-  me = api.get_followers()
-  return me
+  users = tweepy.Cursor(
+      api.get_followers, screen_name=username, count=200).items()
+  follower = []
+  i = 0
+  while True:
+    try:
+      user = next(users)
+    except tweepy.errors.TweepyException:
+      time.sleep(60*15)
+      user = next(users)
+    except StopIteration:
+      break
+    print(i, ' ', user._json)
+    i += 1
+    # follower.append(user._json)
+
+def getFollowers_by_id(userID):
+  api = getAPIV1()
+
 
 # tweets = getTweets('nft')
 # json_object = json.loads(tweets[0]._json)
@@ -64,8 +77,48 @@ def getRecentTweeter(topic, filename):
             person['screen_name'], person['location'], person['description'], person['followers_count'], person['friends_count'], person['listed_count'], person['created_at'], person['favourites_count'], person['time_zone'], person['verified'], person['statuses_count'], person['lang'], person['following'], person['follow_request_sent']]
     # check if this person is already in the database
     idList = helper.readCol(filename, 'id')
-    if str(person['id']) not in idList:
+    # check if the person is in our blacklist
+    blackList = helper.readCol('./data/blacklist.csv', 'id')
+    # check if we already followed this person or already sent a request
+    if str(person['id']) not in idList and person['following'] == False and person['follow_request_sent'] == False and str(person['id']) not in blackList:
       helper.writeToFile(filename, data)
 
+def followUser(userID):
+  client = getClientV2()
+  client.follow_user(target_user_id=userID)
 
-getRecentTweeter('nft', './data/people.csv')
+def sendDirectMessage(userID, message):
+  api = getAPIV1()
+  options = [
+      {
+          "label": "I'm good",
+          "description": "It means you're doing good",
+          "metadata": "external_id_1"
+      },
+      {
+          "label": "Not so good",
+          "description": "It means you're not doing good",
+          "metadata": "external_id_2"
+      }
+  ]
+  try:
+    api.send_direct_message(recipient_id=userID, text=message, quick_reply_options=options)
+  except Exception as e:
+    print(e)
+
+def followAndHello(filename):
+  # run through the list and get all there ids
+  idList = helper.readCol(filename, 'id')
+  # check if already followed these people
+
+  # check if already sent a follow request
+  # follow these people
+  # send direct message to these people
+  # follow every 30 seconds
+
+
+# sendDirectMessage('1504937972453371908', "hello Peter!")
+# followUser('1504696465918111744')
+# print(getUserInfo('ChenHaifan3'))
+# getRecentTweeter('nft','./data/people.csv')
+print(getFollowers_by_username('rmaxb96'))
